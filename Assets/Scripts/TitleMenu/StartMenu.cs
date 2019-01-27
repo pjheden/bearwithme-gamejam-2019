1397b31f5be1b164f1338fbbd1441f0b29c4700a
Julian Hedén <julianheden@gmail.com>
2019-01-27 11:00:37 +0100

    Add intro scenes

---
+++ Assets/Scripts/TitleMenu/StartMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public Button startButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(NextScene);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void NextScene()
+    {
+        SceneManager.LoadScene("StoryScene");
+    }
+}
